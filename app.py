d9cd3d55005e42b3a6a787bdf539f70fd39bef7e
kayo <kayomelese4@gmail.com>
2024-02-23 14:18:49 +0300

    updated new model

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ import joblib
 import streamlit as st
 import pandas as pd
 import numpy as np
-df= pd.read_csv('notencoded.csv', index_col=0)
+df= pd.read_csv('filtercolsmain.csv', index_col=0)
 # Load the label encoders and pipeline from joblib files
-label_encoders = joblib.load('label_encoder_mappings__check1.joblib')
-rf_model = joblib.load('multi_output_rf_model_check1.joblib')
+label_encoders = joblib.load('label_encoders.joblib')
+rf_model = joblib.load('multi_output_rf_model__check123.joblib')
 import streamlit as st
 # Create a unique set of player names
-
 
  # Create mapping dictionaries for dropdowns
 team_abbreviation_to_id = { 'KKR': 4341, 'RCB': 4340, 'RR': 4345,
@@ -17,8 +16,8 @@ team_abbreviation_to_id = { 'KKR': 4341, 'RCB': 4340, 'RR': 4345,
 
 # Define the columns to be label encoded
 columns_to_encode = ['floodlit_name', 'team1_abbreviation', 'team2_abbreviation',
-       'weather_location_code', 'description', 'name',
-       'position', 'ground_name', 'condition']
+                    'description', 'name',
+                    'position', 'ground_name', 'condition']
 
 def apply_label_encoding(df, cols_to_encode, label_encoders):
     for col in cols_to_encode:
@@ -29,14 +28,14 @@ def apply_label_encoding(df, cols_to_encode, label_encoders):
 
 
 def main():
-  #  st.title('Model Prediction App')
-     print("Initialized main")
+    st.title('Model Prediction App')
+
     # Sidebar with user inputs
-  #  st.sidebar.header('Enter Input Data')
+    st.sidebar.header('Enter Input Data')
 # Load the encodings from the joblib file
     
 
-all_encodings = joblib.load('label_encoder_mappings__check1.joblib')
+all_encodings = joblib.load('label_encoders.joblib')
 
 # Get all keys from the dictionary
 all_keys = {key: all_encodings[key].classes_.tolist() for key in all_encodings}
@@ -55,25 +54,31 @@ new_data = {
     'home_team_id': st.sidebar.selectbox('Home Team ID', list(team_abbreviation_to_id.keys())),
     'team1_abbreviation': st.sidebar.selectbox('Team 1 Abbreviation', all_keys['team1_abbreviation']),
     'team2_abbreviation': st.sidebar.selectbox('Team 2 Abbreviation', all_keys['team2_abbreviation']),
-    'weather_location_code': st.sidebar.selectbox('weather code', all_keys['weather_location_code']),
-    'daily_will_it_rain': st.sidebar.number_input('will it rain', min_value=0),
     'batting_position': st.sidebar.number_input('Batting Position', min_value=0),
     'innings_number': st.sidebar.number_input('Innings Number', min_value=0),
     'description': st.sidebar.selectbox('Description', all_keys['description']),
     'age': st.sidebar.number_input('age',value=int(selected_player_info['age']), min_value=0),
-    'Avg_Balls_Faced':int(selected_player_info['Avg_Balls_Faced']),
-    'Avg_Strike_Rate':int(selected_player_info['Avg_Strike_Rate']), 
     'name': st.sidebar.selectbox('Name', all_keys['name']),
     'position': st.sidebar.selectbox('Position', all_keys['position']),
     'ground_name': st.sidebar.selectbox('Town Name', all_keys['ground_name']),
     'avgtemp_c': st.sidebar.number_input('Average Temperature (C)', min_value=0.0),
     'maxwind_kph': st.sidebar.number_input('Max Wind Speed (kph)', min_value=0.0),
+    'Total_Fifties':st.sidebar.number_input('Total Fifties', value=int(selected_player_info['Total_Fifties']), min_value=0),
     'condition': st.sidebar.selectbox('Condition', all_keys['condition']),
+    'Total_Hundreds':st.sidebar.number_input('Total Hundreds', value=int(selected_player_info['Total_Hundreds']), min_value=0),
+    'Avg_Balls_Faced':st.sidebar.number_input('Avg Balls Faced', value=int(selected_player_info['Avg_Balls_Faced']), min_value=0),
+    'Avg_Strike_Rate':st.sidebar.number_input('Avg Strike Rate', value=int(selected_player_info['Avg_Strike_Rate']), min_value=0),
+    'Total_Fours_mean':st.sidebar.number_input('Total Fours Means', value=int(selected_player_info['Total_Fours_mean']), min_value=0),
+    'Total_Sixes_mean':st.sidebar.number_input('Total Sixes Means', value=int(selected_player_info['Total_Sixes_mean']), min_value=0),
+    'Total_runs':st.sidebar.number_input('Total Runs', value=int(selected_player_info['Total_runs']), min_value=0),
+
 
 }
 
 #    # Convert user input dictionary to a DataFrame
 user_input_df = pd.DataFrame([new_data])
+user_input_df['away_team_id'] = user_input_df['away_team_id'].apply(lambda x: team_abbreviation_to_id.get(x, x))
+user_input_df['home_team_id'] = user_input_df['home_team_id'].apply(lambda x: team_abbreviation_to_id.get(x, x))
 
     # Apply label encodings to selected columns
 user_input_df_encoded = apply_label_encoding(user_input_df.copy(), columns_to_encode, label_encoders)
@@ -84,7 +89,6 @@ prediction = rf_model.predict(user_input_df_encoded)
 rounded_prediction = np.round(prediction[0]).astype(int)
 
 # Display the rounded prediction
-# st.write(f'Model Prediction: {rounded_prediction}')
 runs, fours, sixes = prediction[0]  # Unpack the values
 
 # Round the values to the nearest whole number
